98f75ffe6fdf83f52b27dec3b12d478c67939849
Hesh-dev <heshanvinuddika@gmail.com>
2023-03-11 16:00:48 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main()
+{
+
+  float sub1, sub2, avarage; //Creating variables
+
+  printf("Enter the marks of first subject : ");
+  scanf("%f", &sub1); //storing the first subject marks
+  printf("Enter the marks of second subject : ");
+  scanf("%f", &sub2); //storing the second subject marks
+
+  avarage = (sub1 + sub2) / 2; //calculating the avarage mark
+
+  printf("Avarge marks of the two subjets is %.2f", avarage); //displaying the avarge mark
+
   return 0;
 }
-
